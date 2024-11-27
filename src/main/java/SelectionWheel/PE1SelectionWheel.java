aa7b8b0760310c30b7eed26c92e90b08c4edfbea
larissabzoliveira <oliveiralarissa1206@gmail.com>
2024-11-27 22:14:01 +0000

    Initial commit

---
+++ src/main/java/SelectionWheel/PE1SelectionWheel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package SelectionWheel;
+
+/**
+ *
+ * @author larissaoliveira
+ */
+public class PE1SelectionWheel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
